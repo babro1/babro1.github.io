a535b2a00c4ca857e411fdda4643da18e8aecb51
babro1 <babro1@noreply.github.io>
2026-02-09 10:47:37 +0100

    bartsimons back

---
+++ bartsimonscomic/random.js
@@ -0,0 +1,14 @@
+const links = [
+    "./harold",
+    "./haroldsequel",
+    "./argument",
+    "./playship"
+];
+function randlink() {
+    const ra = Math.floor(Math.random() * links.length);
+    return links[ra];
+}
+document.getElementById('radd').addEventListener('click', function() {
+    const randomLink = randlink();
+    window.location.href = randomLink+".html"; // Redirect to the random link
+});
